6faa94fda164782869f8e5dc368beda9e16f69ea
maromo71 <professormoraes@gmail.com>
2022-07-29 14:19:30 -0300

    Up 12

--- projetoSpring/src/main/java/net/maromo/projetospring/web/controller/DepartamentoController.java
+++ projetoSpring/src/main/java/net/maromo/projetospring/web/controller/DepartamentoController.java
@@ -1,18 +1,34 @@
 package net.maromo.projetospring.web.controller;
 
+import net.maromo.projetospring.domain.Departamento;
+import net.maromo.projetospring.service.DepartamentoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/departamentos")
 public class DepartamentoController {
+    private final DepartamentoService service;
+
+    public DepartamentoController(DepartamentoService service) {
+        this.service = service;
+    }
+
     @GetMapping("/cadastrar")
-    public String cadastrar(){
+    public String cadastrar(Departamento departamento){
         return "/departamento/cadastro";
     }
     @GetMapping("/listar")
     public String listar(){
         return "/departamento/lista";
+    }
+
+    @PostMapping("/salvar")
+    public String salvar(Departamento departamento){
+        service.salvar(departamento);
+        return "redirect:/departamentos/cadastrar";
     }
 }
